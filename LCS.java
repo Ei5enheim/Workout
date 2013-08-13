6551f0568c5aec27d761955ff0e5dfaf7cd4b67e
gopidirajesh <rajesh@cs.unc.edu>
2013-08-13 01:00:52 -0400

    Longest common seq

---
+++ LCS.java
@@ -0,0 +1,88 @@
+/**
+ * Author: Rajesh Gopidi
+ *
+ */
+
+import java.util.LinkedList;
+
+public class LCS 
+{
+    private static int I = 1024;
+    private static int J = 1;
+    private static int POW = 10;
+    private int[][] length;
+    private int[][] indices;
+    private int[] seq1, seq2;
+
+    public LCS ()
+    {
+
+    }
+
+    public LCS (int[] seq1, int[] seq2)
+    {
+        this.seq1 = seq1;
+        this.seq2 = seq2;
+        length = new int[seq1.length][seq2.length];
+        indices = new int[seq1.length][seq2.length];
+    }
+
+    public void computeLCS()
+    {
+        for (int i = 1; i < seq1.length; i++) {
+            for (int j = 1; j < seq2.length; j++) {
+                if (seq1[i] == seq2[j]) {
+                    length[i][j] = length[i-1][j-1] + 1;
+                    indices[i][j] = I * (i-1) + j-1;
+                } else if (length[i-1][j] >= length[i][j-1]) {
+                    length[i][j] = length[i-1][j];
+                    indices[i][j] = -(I * (i-1) + j); 
+                } else {
+                    length[i][j] = length[i][j-1];
+                    indices[i][j] =  -(I * (i) + j-1);
+                }
+            }
+        }
+    }
+
+    public void displayLCS()
+    {
+        System.out.println("Length of Longest Common Sequence is "+
+                            length[seq1.length-1][seq2.length-1]);
+
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        int row = seq1.length-1, col = seq2.length - 1, temp = -1, index = -1;
+        while ((row != 0) && (col != 0)) {
+            temp = indices[row] [col];
+            if (temp < 0)
+                index = -temp;
+            else
+                index = temp;
+
+            if (temp > 0) 
+                list.addFirst(seq1[row]);
+            
+            col = (((1<< POW) -1) & index) & 0x7FFFFFFF;
+            row = index >> POW; 
+
+        }
+        Integer i = list.poll();
+    
+        while (i != null) {
+            System.out.print(i);
+            i = list.poll();
+        }
+
+        System.out.println();
+    }
+
+    public static void main (String[] args)
+    {
+        int[] seq1 = {0,1,2,3,2,4,1,2}; 
+        int[] seq2 = {0,2,4,3,1,2,1};
+
+        LCS obj = new LCS(seq1, seq2);
+        obj.computeLCS();
+        obj.displayLCS();
+    }
+}
